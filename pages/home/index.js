cb8986eb075ba5b6087faeb7916ff18f8f15d669
broveloper <broveloper@gmail.com>
2020-09-20 19:20:04 -1000

    change name

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,8 +15,8 @@ export default function Home() {
     </section>
     <MediaCopySection
       button={{
-        label: 'More Opportunities',
-        href: '/opportunities',
+        label: 'See the vision',
+        href: '/vision',
       }}
       description="Software engineering has established itself as an integral part of every industry in our world today. It has proven itself to be pandemic- and recession-proof, and its future is bright. Over the last decade, software bootcamps have popped up all over the Mainland. These bootcamps are 3- to 4-month immersive programs designed to produce job-ready graduates to supply the ever-growing demand for software engineers at companies like Google, Facebook, Amazon, and many others. The only requirements for success are education, equipment, an internet connection, and ambition."
       media={<img src="/mobileapp.png" style={{ width: '100%'}}/>}
